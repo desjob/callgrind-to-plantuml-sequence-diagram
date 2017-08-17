5e0128be8ea0814a73b920c048ed880d659d836e
Ana Azevedo <ana.azevedo@medicore.nl>
2017-08-17 18:14:51 +0200

    Add SequenceBuilder

---
+++ src/SequenceDiagram/SequenceBuilder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace CallgrindToPlantUML\SequenceDiagram;
+
+use CallgrindToPlantUML\Callgrind\CallQueueIndex;
+use CallgrindToPlantUML\SequenceDiagram\Call as SequenceCall;
+
+class SequenceBuilder
+{
+    const ACTOR = 'actor';
+    const RETURN = 'return';
+
+    /** @var \CallgrindToPlantUML\Callgrind\CallQueueIndex */
+    private $events;
+
+    /** @var \CallgrindToPlantUML\Callgrind\Call[] */
+    private $summary;
+
+    /** @var \CallgrindToPlantUML\SequenceDiagram\Sequence */
+    private $sequence;
+
+    /**
+     * @param \CallgrindToPlantUML\Callgrind\CallQueueIndex $events
+     * @param \CallgrindToPlantUML\Callgrind\Call[] $summary
+     */
+    public function __construct(CallQueueIndex $events, array $summary)
+    {
+        $this->events = $events;
+        $this->summary = $summary;
+    }
+
+    /**
+     * @return \CallgrindToPlantUML\SequenceDiagram\Sequence
+     */
+    public function build(): Sequence
+    {
+        $sequence = new Sequence();
+        foreach ($this->summary as $summary) {
+            $this->getChildren($summary->getId(), static::ACTOR, $sequence);
+        }
+
+        return $sequence;
+    }
+
+    /**
+     * Create Sequence Call for the current node and call recursevely for the children.
+     *
+     * @param int $fromId
+     * @param string $fromClass
+     * @param \CallgrindToPlantUML\SequenceDiagram\Sequence $sequence
+     */
+    private function getChildren(int $fromId, string $fromClass, Sequence &$sequence)
+    {
+        $callQueue = $this->events->get($fromId);
+        $call = $callQueue->pop();
+
+        $sequenceCall = new SequenceCall($fromClass, $call->getToClass(), $call->getMethod());
+        $sequence->add($sequenceCall);
+
+        if (!empty($call->getSubCallIds())) {
+            foreach ($call->getSubCallIds() as $subCallId) {
+                $this->getChildren($subCallId, $call->getToClass(), $sequence);
+            }
+            $sequenceCall = new SequenceCall($fromClass, $call->getToClass(), static::RETURN);
+            $sequence->add($sequenceCall);
+        } else {
+            $sequenceCall = new SequenceCall($fromClass, $call->getToClass(), static::RETURN);
+            $sequence->add($sequenceCall);
+        }
+    }
+}
---
+++ tests/SequenceDiagram/SequenceBuilderTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace CallgrindToPlantUML\Tests\SequenceDiagram;
+
+use CallgrindToPlantUML\Callgrind\Call;
+use CallgrindToPlantUML\Callgrind\CallQueue;
+use CallgrindToPlantUML\Callgrind\CallQueueIndex;
+use CallgrindToPlantUML\SequenceDiagram\SequenceBuilder;
+use PHPUnit\Framework\TestCase;
+
+class SequenceBuilderTest extends TestCase
+{
+    /** @var \CallgrindToPlantUML\Callgrind\CallQueueIndex */
+    private $events;
+
+    /** @var \CallgrindToPlantUML\Callgrind\Call[] */
+    private $summary;
+
+    protected function setUp()
+    {
+        $callQueueIndex = new CallQueueIndex();
+        $callQueue = new CallQueue();
+        $callQueue->add(new Call(1, 'User', '__construct', array()));
+        $callQueue->add(new Call(1, 'User', '__construct', array()));
+        $callQueue->add(new Call(1, 'User', '__construct', array()));
+        $callQueueIndex->add(1, $callQueue);
+
+        $callQueue = new CallQueue();
+        $callQueue->add(new Call(2, 'UserService', '__construct', array(1, 1, 1)));
+        $callQueueIndex->add(2, $callQueue);
+
+        $callQueue = new CallQueue();
+        $callQueue->add(new Call(3, 'Controller', '__construct', array()));
+        $callQueueIndex->add(3, $callQueue);
+
+        $callQueue = new CallQueue();
+        $callQueue->add(new Call(4, 'UserService', 'findUser', array()));
+        $callQueue->add(new Call(4, 'UserService', 'findUser', array()));
+        $callQueue->add(new Call(4, 'UserService', 'findUser', array()));
+        $callQueue->add(new Call(4, 'UserService', 'findUser', array()));
+        $callQueue->add(new Call(4, 'UserService', 'findUser', array()));
+        $callQueue->add(new Call(4, 'UserService', 'findUser', array()));
+        $callQueue->add(new Call(4, 'UserService', 'findUser', array()));
+        $callQueue->add(new Call(4, 'UserService', 'findUser', array(7)));
+        $callQueueIndex->add(4, $callQueue);
+
+        $callQueue = new CallQueue();
+        $callQueue->add(new Call(5, 'User', '__toString', array()));
+        $callQueue->add(new Call(5, 'User', '__toString', array()));
+        $callQueue->add(new Call(5, 'User', '__toString', array()));
+        $callQueue->add(new Call(5, 'User', '__toString', array()));
+        $callQueue->add(new Call(5, 'User', '__toString', array()));
+        $callQueue->add(new Call(5, 'User', '__toString', array()));
+        $callQueue->add(new Call(5, 'User', '__toString', array()));
+        $callQueueIndex->add(5, $callQueue);
+
+        $callQueue = new CallQueue();
+        $callQueue->add(new Call(6, 'Controller', 'execute', array(4, 5, 4, 5)));
+        $callQueue->add(new Call(6, 'Controller', 'execute', array(4, 5)));
+        $callQueue->add(new Call(6, 'Controller', 'execute', array(4, 5, 4, 5, 4, 5, 4, 5)));
+        $callQueue->add(new Call(6, 'Controller', 'execute', array(4, 8)));
+        $callQueueIndex->add(6, $callQueue);
+
+        $callQueue = new CallQueue();
+        $callQueue->add(new Call(7, 'php::Exception', '__construct', array()));
+        $callQueueIndex->add(7, $callQueue);
+
+        $callQueue = new CallQueue();
+        $callQueue->add(new Call(8, 'php::Exception', 'getMessage', array()));
+        $callQueueIndex->add(8, $callQueue);
+
+        $callQueue = new CallQueue();
+        $callQueue->add(new Call(9, '', '{main}', array()));
+        $callQueueIndex->add(9, $callQueue);
+
+        $this->events = $callQueueIndex;
+
+        $this->summary = [];
+        $this->summary[] = new Call(2, 'UserService', '__construct', array());
+        $this->summary[] = new Call(3, 'Controller', '__construct', array());
+        $this->summary[] = new Call(6, 'Controller', 'execute', array());
+        $this->summary[] = new Call(6, 'Controller', 'execute', array());
+        $this->summary[] = new Call(6, 'Controller', 'execute', array());
+        $this->summary[] = new Call(6, 'Controller', 'execute', array());
+    }
+
+    /**
+     * @testdox Test some of the flow.
+     */
+    public function test()
+    {
+        $sequenceBuilder = new SequenceBuilder($this->events, $this->summary);
+        $result = $sequenceBuilder->build();
+
+        $call = $result->pop();
+        $this->assertSame(SequenceBuilder::ACTOR, $call->getFromClass());
+        $this->assertSame('UserService', $call->getToClass());
+        $this->assertSame('__construct', $call->getMethod());
+
+        $call = $result->pop();
+        $this->assertSame('UserService', $call->getFromClass());
+        $this->assertSame('User', $call->getToClass());
+        $this->assertSame('__construct', $call->getMethod());
+
+        $call = $result->pop();
+        $this->assertSame('UserService', $call->getFromClass());
+        $this->assertSame('User', $call->getToClass());
+        $this->assertSame(SequenceBuilder::RETURN, $call->getMethod());
+
+        $call = $result->pop();
+        $this->assertSame('UserService', $call->getFromClass());
+        $this->assertSame('User', $call->getToClass());
+        $this->assertSame('__construct', $call->getMethod());
+
+        $call = $result->pop();
+        $this->assertSame('UserService', $call->getFromClass());
+        $this->assertSame('User', $call->getToClass());
+        $this->assertSame(SequenceBuilder::RETURN, $call->getMethod());
+
+        $call = $result->pop();
+        $this->assertSame('UserService', $call->getFromClass());
+        $this->assertSame('User', $call->getToClass());
+        $this->assertSame('__construct', $call->getMethod());
+
+        $call = $result->pop();
+        $this->assertSame('UserService', $call->getFromClass());
+        $this->assertSame('User', $call->getToClass());
+        $this->assertSame(SequenceBuilder::RETURN, $call->getMethod());
+
+        $call = $result->pop();
+        $this->assertSame(SequenceBuilder::ACTOR, $call->getFromClass());
+        $this->assertSame('UserService', $call->getToClass());
+        $this->assertSame(SequenceBuilder::RETURN, $call->getMethod());
+    }
+}
