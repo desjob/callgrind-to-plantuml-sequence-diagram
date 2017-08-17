d3791ff41d3898cf79e795c3de21721682cf3d5d
Job 'desjob' Schipper <job.schipper@medicore.nl>
2017-08-17 18:14:15 +0200

    added PlantUML formatter

---
+++ src/PlantUML/Formatter.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace CallgrindToPlantUML\PlantUML;
+
+use CallgrindToPlantUML\SequenceDiagram\Call;
+
+class Formatter
+{
+    public function format(Call $call): string
+    {
+        return $call->getFromClass().' -> '.$call->getToClass();
+    }
+}
