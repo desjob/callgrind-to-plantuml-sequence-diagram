c4c8c599e49833a7579f16cade1575f900e2aa58
Job 'desjob' Schipper <job.schipper@medicore.nl>
2017-08-22 17:06:27 +0200

    added filter class StartFromFilter

---
+++ src/SequenceDiagram/Filter/StartFromFilter.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace CallgrindToPlantUML\SequenceDiagram\Filter;
+
+use CallgrindToPlantUML\Callgrind\Parser;
+use CallgrindToPlantUML\SequenceDiagram\Sequence;
+
+class StartFromFilter implements FilterInterface
+{
+    /** @var string */
+    private $toClass;
+
+    /** @var string */
+    private $method;
+
+    /**
+     * @param string $toClass
+     * @param string $method
+     */
+    public function __construct(string $toClass, string $method)
+    {
+        $this->toClass = $toClass;
+        $this->method = $method;
+    }
+
+    /**
+     * @param \CallgrindToPlantUML\SequenceDiagram\Sequence $sequence
+     *
+     * @return \CallgrindToPlantUML\SequenceDiagram\Sequence
+     */
+    public function apply(Sequence $sequence): Sequence
+    {
+        $startAdding = false;
+        $startedFromCall = null;
+        $filteredSequence = new Sequence();
+        while ($sequence->hasItems()) {
+            $call = $sequence->pop();
+
+            // check if we need to start adding yet
+            if(!$startAdding && $call->getToClass() === $this->toClass && $call->getMethod() === $this->method) {
+                $startAdding = true;
+                $startedFromCall = $call;
+            }
+
+            if(!$startAdding) {
+                continue;
+            } else {
+                $filteredSequence->add($call);
+
+                //once we reach the returncall of the $startedFromCall, stop adding
+                if($call->isReturnCall() && $call->getToClass() === $startedFromCall->getFromClass() && $call->getFromClass() === $startedFromCall->getToClass()) {
+                    $startAdding = false;
+                    $startedFromCall = null;
+                }
+            }
+        }
+
+        return $filteredSequence;
+    }
+}
